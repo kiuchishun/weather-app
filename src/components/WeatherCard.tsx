6ae912c90c151dd70d385149e60ffd182b1a9e3f
shun4559 <kiuchi4559@gmail.com>
2026-04-02 18:34:00 +0900

    整形

--- src/components/WeatherCard.tsx
+++ src/components/WeatherCard.tsx
@@ -1,18 +1,19 @@
-import type { WeatherData } from "../App"
-type Props ={
-  weatherData:WeatherData
-}
+import type { WeatherData } from "../App";
+type Props = {
+  weatherData: WeatherData;
+};
 
-
-export default function ({weatherData}:Props){
-  return (   <div>
-          <h3>お天気情報：{weatherData.location}</h3>
-          <p>気温 : {weatherData.temperature}℃</p>
-          <p>湿度 : {weatherData.humidity}%</p>
-          <p>風速 : {weatherData.windSpeed}m/s</p>
-          <img
-            src={`https://openweathermap.org/img/wn/${weatherData.icon}@2x.png`}
-            alt="weather icon"
-          ></img>
-        </div>)
+export default function ({ weatherData }: Props) {
+  return (
+    <div>
+      <h3>お天気情報：{weatherData.location}</h3>
+      <p>気温 : {weatherData.temperature}℃</p>
+      <p>湿度 : {weatherData.humidity}%</p>
+      <p>風速 : {weatherData.windSpeed}m/s</p>
+      <img
+        src={`https://openweathermap.org/img/wn/${weatherData.icon}@2x.png`}
+        alt="weather icon"
+      ></img>
+    </div>
+  );
 }
